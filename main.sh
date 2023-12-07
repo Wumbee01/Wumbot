a0e69f845f4fa786e43d9f3c2a9cdc9e80d2d421
Wumbee <124483092+Wumbee01@users.noreply.github.com>
2023-12-07 19:24:12 +0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,13 +3,13 @@ git checkout sub
 git_sync () {
         git checkout main
 	git remote set-url origin https://${TOKEN}@github.com/Wumbee01/Wumbot.git
-        git merge sub
 	git add --all
 	git config --global user.email "not.real@fake-email.com"
 	git config --global user.name "Wumbee"
 	git commit -am 'sync main'
 	git config pull.rebase false
-	git push origin main
+        git pull origin main
+        git push origin main:sub --force
 }
 
 fetch_updates () {
