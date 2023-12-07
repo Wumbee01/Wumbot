810440b6b565a76c0b5c41a3476fcf14aac1b212
Wumbee <124483092+Wumbee01@users.noreply.github.com>
2023-12-07 18:20:31 +0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 git checkout sub
 git_sync () {
+        git checkout sub
 	git add --all
 	git config --global user.email "not.real@fake-email.com"
 	git config --global user.name "Wumbee"
 	git commit -am 'sync main'
 	git config pull.rebase false
-	git push https://Wumbee01:${GIT}@github.com/${REPO}.git main
+	git origin sub:main
 }
 
 touch BOTCONDITION
