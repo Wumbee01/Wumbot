4c1dcaa97d46d27538a428c5f46e8823866ea10d
Wumbee <124483092+Wumbee01@users.noreply.github.com>
2024-01-19 16:39:39 +0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,6 +3,7 @@ docker build -t command-runner .
 git checkout sub
 git_sync () {
         git checkout main
+	rm disabled.txt
 	git remote set-url origin https://${GIT}@github.com/Wumbee01/Wumbot.git
 	git add --all
 	git config --global user.email "not.real@fake-email.com"
@@ -25,7 +26,6 @@ izuku () {
 	  do
 	  mkdir -p logs
 	  python3 src/main.py
-          rm -rf disabled.txt
 	  git_sync
           fetch_updates
 	  done
